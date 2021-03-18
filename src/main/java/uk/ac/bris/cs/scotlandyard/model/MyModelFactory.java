2141566662dbe81d1e393f0b68f64e8e85077493
mateuszwlo <mateuszwlodarczyk2002@gmail.com>
2021-03-18 21:47:15 +0000

    DONE!

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,7 +4,11 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
+import org.checkerframework.checker.units.qual.A;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.ArrayList;
 
 /**
  * cw-model
@@ -15,7 +19,52 @@ public final class MyModelFactory implements Factory<Model> {
 	@Nonnull @Override public Model build(GameSetup setup,
 	                                      Player mrX,
 	                                      ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Implement me!");
+		return new Model() {
+			private Board.GameState gameState = new MyGameStateFactory().build(setup, mrX, detectives);
+			private ImmutableSet<Observer> observers = ImmutableSet.of();
+
+			@Nonnull
+			@Override
+			public Board getCurrentBoard() {
+				return gameState;
+			}
+
+			@Override
+			public void registerObserver(@Nonnull Observer observer) {
+				final ArrayList<Observer> newObservers = new ArrayList<>(observers);
+				if(!newObservers.contains(observer)) newObservers.add(observer);
+				else throw new IllegalArgumentException();
+				observers = ImmutableSet.copyOf(newObservers);
+			}
+
+			@Override
+			public void unregisterObserver(@Nonnull Observer observer) {
+				if(observer == null) throw new NullPointerException();
+				final ArrayList<Observer> newObservers = new ArrayList<>(observers);
+				if(newObservers.contains(observer)) newObservers.remove(observer);
+				else throw new IllegalArgumentException();
+				observers = ImmutableSet.copyOf(newObservers);
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Observer> getObservers() {
+				return observers;
+			}
+
+			@Override
+			public void chooseMove(@Nonnull Move move) {
+				gameState = gameState.advance(move);
+				for (Observer o: observers) {
+					o.onModelChanged(gameState, Observer.Event.MOVE_MADE);
+				}
+
+				if(!gameState.getWinner().isEmpty()){
+					for (Observer o: observers) {
+						o.onModelChanged(gameState, Observer.Event.GAME_OVER);
+					}
+				}
+			}
+		};
 	}
 }
